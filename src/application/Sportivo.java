ca2d837dc50558317fa4927898206b94d1c51145
Lorenzo <Lorenzo@Lorenzo-PC.lan>
2018-12-18 21:31:18 +0100

    Prima versione

---
+++ src/application/Sportivo.java
@@ -0,0 +1,41 @@
+package application;
+
+public class Sportivo {
+
+	private String nome;
+	private String cognome;
+	private String codiceFiscale;
+	private String disciplina;
+	public Sportivo(String nome, String cognome, String codiceFiscale, String disciplina) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codiceFiscale = codiceFiscale;
+		this.disciplina = disciplina;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getCodiceFiscale() {
+		return codiceFiscale;
+	}
+	public void setCodiceFiscale(String codiceFiscale) {
+		this.codiceFiscale = codiceFiscale;
+	}
+	public String getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+	
+}
